a44c0fdf3cb3f182545f2a8e99108c8b335d66a9
RANA Hamza <253780593+ranag786tech-lang@users.noreply.github.com>
2026-09-06 09:44:17 +0500

    Delete preload.js

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron')
-
-contextBridge.exposeInMainWorld('digidAPI', {
-  chat: (prompt, agent) => ipcRenderer.invoke('ai-chat', { prompt, agent }),
-  setToken: (token) => ipcRenderer.send('set-hf-token', token)
-})
